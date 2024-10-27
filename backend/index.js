f749e725d3a8c8ee9c5cb8c1f8d9ceef1d6e46c0
Kathan0611 <adalajakathan06@gmail.com>
2024-10-28 03:29:36 +0530

    update route

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookiesParser());
 
-app.use('api/v1',router);
+app.use('/api/v1',router);
 
 connect().then(()=>{
     app.listen(5000,()=>{
