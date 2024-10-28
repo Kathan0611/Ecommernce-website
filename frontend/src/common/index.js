f589502bcef9fc2439941ff6d19e7b63d96d1af7
Kathan0611 <adalajakathan06@gmail.com>
2024-10-28 09:49:35 +0530

    lastest

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomain=process.env.Backend_Url
+const backendDomain=`https://ecommernce-website.onrender.com`
 const SummaryApi = {
      Signup:{
         url:`${backendDomain}/api/v1/signup`,
--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -15,6 +15,8 @@ const CategoryList = () => {
             method:SummaryApi.categoryProduct.method
         })
         setloading(false)
+        console.log(SummaryApi)
+        console.log(categoryProduct.url)
         const dataResponse=await repsonse.json();
         setCategoryProduct(dataResponse.data);
     }
